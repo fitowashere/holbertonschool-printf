bdd2f411809801da1caa97f74371490a2381f6ce
CaloMorell <oscarmorell07@yahoo.com>
2023-07-28 23:21:55 +0000

    prints binary

--- binary.c
+++ binary.c
@@ -10,32 +10,32 @@
 
 int printf_binary(unsigned int num, char *buffer, int printed)
 {
-    int binary[32] = {0};
-    int i = 0;
+int binary[32] = {0};
+int i = 0;
 
-    if (num == 0)
-    {
-        buffer[0] = '0';
-        buffer[1] = '\0';
-        printed++;
-        return (printed);
-    }
+if (num == 0)
+{
+buffer[0] = '0';
+buffer[1] = '\0';
+printed++;
+return (printed);
+}
 
-    while (num > 0)
-    {
-        binary[i] = num % 2;
-        num /= 2;
-        i++;
-    }
+while (num > 0)
+{
+binary[i] = num % 2;
+num /= 2;
+i++;
+}
 
-    while (i > 0)
-    {
-        i--;
-        buffer[printed] = '0' + binary[i];
-        printed++;
-    }
+while (i > 0)
+{
+i--;
+buffer[printed] = '0' + binary[i];
+printed++;
+}
 
-    buffer[printed] = '\0';
-    return (printed);
+buffer[printed] = '\0';
+return (printed);
 }
 
